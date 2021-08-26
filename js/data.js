878f5afe715de587ab2dcea0c2593822691677dd
Wagner Ferreira <wcarvalho.ferreira@gmail.com>
2021-08-26 16:20:33 -0300

    initial commit

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const db = [
+    {category: "Frutas",
+     words: ["banana", "pera", "manga", "uva", "ameixa", "graviola", "jabuticaba", "laranja", "abacate", "acerola", "caju", "kiwi", "morango"]
+    },
+    {category: "País",
+     words: ["brasil", "angola", "frança", "tunisia", "marrocos", "italia", "portugal", "libia", "china", "argentina", "venezuela", "peru"]
+    },
+    {category: "Animais",
+     words: ["abelha", "boi", "cavalo", "drometario", "elefante", "macaco", "gorila", "girafa", "pantera", "rato", "suricate", "urso", "porco"]
+    }
+]
+
+
+const images = new Map([
+                       [1, "images/1_homem_ferro.png"],
+                       [2, "images/2_homem_ferro.png"],
+                       [3, "images/3_homem_ferro.png"],
+                       [4, "images/4_homem_ferro.png"],
+                       [5, "images/5_homem_ferro.png"],
+                       [6, "images/6_homem_ferro.png"]])
